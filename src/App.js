7e9a622ccc308c9b8af127957da9c57f266482e2
amandascam03 <amandascam03@gmail.com>
2024-11-25 16:22:14 -0300

    adicionando app bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SearchAppBar from "./components/SearchAppBar/SearchAppBar";
+// import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchAppBar />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
